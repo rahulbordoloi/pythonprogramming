a41a38a74ede000dc6575df9345ac17919b9472f
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-07-07 08:56:24 +0530

    Time Conversion

---
+++ Hackerrank Practice/Time Conversion.py
@@ -0,0 +1,66 @@
+'''
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+s: a string representing time in  hour format
+Input Format
+
+A single string  containing a time in -hour clock format (i.e.:  or ), where  and .
+
+Constraints
+
+All input times are valid
+Output Format
+
+Convert and print the given time in -hour format, where .
+
+Sample Input 0
+
+07:05:45PM
+Sample Output 0
+
+19:05:45
+'''
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s):
+    #
+    # Write your code here.
+    #
+    
+    time = s.split(":")
+    if s[-2:] == "PM":
+        if time[0] != "12":
+            time[0] = str(int(time[0])+12)
+    else:
+        if time[0] == "12":
+            time[0] = "00"
+    ntime = ':'.join(time)
+    return str(ntime[:-2])
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    print(s)
+
+    result = timeConversion(s)
+
+    f.write(result + '\n')
+
+    f.close()
