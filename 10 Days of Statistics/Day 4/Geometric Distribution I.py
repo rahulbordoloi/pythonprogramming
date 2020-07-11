4b4af42cc21acc60d1140240e3d3a19ac5015b99
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-07-11 12:49:28 +0530

    Geometric Distribution I

---
+++ 10 Days of Statistics/Day 4/Geometric Distribution I.py
@@ -0,0 +1,27 @@
+'''
+Objective 
+In this challenge, we learn about geometric distributions. Check out the Tutorial tab for learning materials!
+
+Task 
+The probability that a machine produces a defective product is . What is the probability that the  defect is found during the  inspection?
+
+Input Format
+
+The first line contains the respective space-separated numerator and denominator for the probability of a defect, and the second line contains the inspection we want the probability of being the first defect for:
+
+1 3
+5
+If you do not wish to read this information from stdin, you can hard-code it into your program.
+
+Output Format
+
+Print a single line denoting the answer, rounded to a scale of  decimal places (i.e.,  format).
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+p = 0.333;
+q = 1.0 - p;
+
+g_prob = pow(q,4)*p;
+print(round(g_prob, 3))
