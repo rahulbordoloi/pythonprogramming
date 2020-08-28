53ab79ee1094e063c40d17f56e29caba3cb33c18
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-08-28 10:24:08 +0530

    Sparse Arrays Problem

---
+++ Hackerrank Practice/sparse_arrays.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    results = []
+    for i in queries:
+        if i not in strings:
+            results.append(0)
+        else:
+            results.append(strings.count(i))
+    return results 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
