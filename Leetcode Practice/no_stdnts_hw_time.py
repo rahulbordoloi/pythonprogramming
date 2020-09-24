95fc0e51190c97020b18e7125f94e18ccfd59b31
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-09-24 07:29:51 +0530

    Number of Students Doing Homework at a Given Time

---
+++ Leetcode Practice/no_stdnts_hw_time.py
@@ -0,0 +1,11 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        
+        c = 0
+        for i in range(len(startTime)):
+            
+            if queryTime in range(startTime[i], endTime[i] + 1):
+                c += 1
+        
+        return c
+        
